b2ea080ba48adaafaf7eadda4a35556608302b92
light1414 <dantata49@gmail.com>
2023-12-14 12:36:21 -0800

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,27 @@
 
 /**
  * bin_to_uint - function changes a binary num to an unsigned int.
- * @b: pointer to a string holding a bin num
+ * @x: pointer to a string holding a bin num
  *
  * Return: unsigned int with decimal numb of bin num or 0 if error
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *x)
 {
 	int j;
 	unsigned int num;
 
 	num = 0;
-	if (!b)
+	if (!x)
 		return (0);
-	for (j = 0; b[j] != '\0'; j++)
+	for (j = 0; x[j] != '\0'; j++)
 	{
-		if (b[j] != '0' && b[j] != '1')
+		if (x[j] != '0' && x[j] != '1')
 			return (0);
 	}
-	for (j = 0; b[j] != '\0'; j++)
+	for (j = 0; x[j] != '\0'; j++)
 	{
 		num <<= 1;
-		if (b[j] == '1')
+		if (x[j] == '1')
 			num += 1;
 	}
 	return (num);
