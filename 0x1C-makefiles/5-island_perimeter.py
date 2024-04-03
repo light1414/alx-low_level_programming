e06db8edd8d464622e1fb38a926b12c71dadbc43
light1414 <dantata49@gmail.com>
2024-04-03 03:18:52 -0700

    update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+ perimeter of island module
+"""
+
+
+def island_perimeter(grid):
+    """prints the perimeter of the island
+    Args:
+         grid (list) = list of integers
+    """
+
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                # checks for boundery or lakes
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
